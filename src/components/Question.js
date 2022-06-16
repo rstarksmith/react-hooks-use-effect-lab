1f5ac9ce9bf263040982b3c52cc426fd32c4d694
Rachel <rstark.smith@gmail.com>
2022-06-16 12:21:15 -0500

    commit tree bbbf911e798ec42f1c751bf38315542ab8e19cf1

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      countdown()
+    }, 1000);
+    return function cleanup() {
+      clearTimeout(timer)
+  }
+})
+
+  const countdown = () => {
+    if(timeRemaining === 0) {
+      setTimeRemaining(10)
+      onAnswered(false)
+    } else {
+      setTimeRemaining(timeRemaining - 1)
+    }
+  }
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
